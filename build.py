93c41676080f894a02f5cd62f4b7ddb71f9a6a7a
David Mindich <DSMindich@gmail.com>
2019-11-16 19:20:14 -0800

    fixed ssg

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-# Functions/Lists
+# Lists
 pages = [
     {
         "filename": "content/index.html",
@@ -19,41 +19,27 @@ pages = [
         "active": "{{active_port}}"
     },
 ]
-    #saves out each webpage
+#Functions
 def main():
-    full_page = apply_base()
-    web_page = page["output"]
+    for page in pages:
+        page_title = page["title"]
+        final_file = page["output"]
+        content_path = page["filename"]
+        active_link = page["active"]
+        open_base()
+        apply_template(content_path,page_title,active_link,final_file)
+    #Opens base template 
+def open_base():
+    base_html = open("templates/base.html").read()
+    return base_html
+    #Adds page titles, active page functionality, and content to base template, saves output to /docs
+def apply_template(content,title,act,web_page):
+    page_content = open(content).read()
+    titled_page = open_base().replace("{{title}}", title)
+    active_page= titled_page.replace(act, "active")
+    full_page = active_page.replace("{{content}}", page_content)
     open(web_page, 'w+').write(full_page)
 
-    #adds the header and footer to each content page
-def apply_base():
-    active_page = apply_active(apply_title())
-    page_name = page["filename"]
-    content = open(page_name).read()
-    full_page = active_page.replace("{{content}}", content)
-    return full_page
-
-    #adds the title to each webpage
-def apply_title():
-    base_html = open("templates/base.html").read()
-    page_tile = page["title"]
-    entitled_page = base_html.replace("{{title}}", page_tile)
-    return entitled_page
-
-    #makes the active page black in the nav
-    #I don't understand the usefulness of using arguemnts with this... although
-    #  I guess maybe I could change the order that these happen if I made them 
-    # all work via arguments? I can update the rest for next homework need to move on
-def apply_active(titled_page):
-    activator = page["active"]
-    active_page= titled_page.replace(activator, "active")
-    return active_page
-
 # Script
-
-for page in pages:
-     apply_title()
-     apply_active(apply_title())
-     apply_base()
-     if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
